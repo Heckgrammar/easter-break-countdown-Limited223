64814f9bd9c71d323970609663bbaaf19046f740
Abdurrahman Abbas <22AAbbas@heckgrammar.co.uk>
2025-04-04 10:15:46 +0100

    gdggd

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            while (true)
+            {
+				DateTime current = DateTime.Now;
+                Console.WriteLine("what is target year");
+                int year = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("what is target month");
+				int month = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("what is target day");
+				int day = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("what is target hour");
+				int hour = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("what is target year");
+				int min = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("what is target secs");
+				int secs = Convert.ToInt32(Console.ReadLine());
+				DateTime target = new DateTime(year, month, day, hour, min, secs);
+                TimeSpan difference = target - current;
+                Console.WriteLine(difference);
+				Thread.Sleep(1000);
+                Console.Clear();
+			}
+           
         }
     }
 }
